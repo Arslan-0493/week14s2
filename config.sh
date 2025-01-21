3e1a104df707b217532e29f07949a5fd02894364
Arslan Fakhir <l1f21bsse0493@ucp.edu.pk>
2025-01-21 11:45:06 +0500

    Initial commit

---
+++ config.sh
@@ -0,0 +1,9 @@
+echo "Going to configure Repo"
+git branch -m develop
+git config --local user.name "Arslan Fakhir"
+git config --local user.email l1f21bsse0493@ucp.edu.pk
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
+
+
